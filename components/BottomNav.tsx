857a3c9e2c5c87690324c91923c57f536a5b6985
bkramarz <105524805+bkramarz@users.noreply.github.com>
2026-06-10 11:25:23 -0700

    Revert "perf: cut initial-load latency with local JWT auth, global preconnect, and shared profile context" (#133)
    This reverts commit 2a0e74175b2d089de0b6f0fe1dc98ddc715fc328.

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { useProfile } from "@/components/ProfileProvider";
+import { useProfile } from "@/hooks/useProfile";
 
 const TABS = [
   {
--- components/MobileHeaderProfile.tsx
+++ components/MobileHeaderProfile.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useProfile } from "@/components/ProfileProvider";
+import { useProfile } from "@/hooks/useProfile";
 import NotificationBell from "./NotificationBell";
 
 export default function MobileHeaderProfile() {
